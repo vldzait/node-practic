8ace02255557b22185acf305a45d842e99e34e88
Владимир Зайцев <zaytsev_va@resanta.ru>
2022-01-12 01:31:00 +0300

    [fixed] node-lesson-1

--- node/1/drawTree.js
+++
@@ -1,3 +0,0 @@
-
-
-module.exports.drawTree = tree;
--- node/1/index.js
+++ node/1/index.js
@@ -1,38 +1,5 @@
 const fs = require('fs');
 
-
-fs.readFile('./data.json', 'utf8', (err, data) => {
-    if (err) {
-        console.log(`Error reading file from disk: ${err}`);
-    } else {
-        const obj = JSON.parse(data);
-        tree(obj, 0);
-    }
-});
-
-// function tree (obj = [], str = 0,) {
-
-//   for (const iterator of obj) {
-//     check(iterator, str);
-//   }
-// }
-
-// function check (item , str = 0) {
-//   if(str === 0) console.log(`${item.name}`);
-//   if(str === 1) console.log(`|__${item.name}`);
-//   if(str > 1) {
-//     let char = '|';
-//     while (char.length < str) {
-//       char += '  ';
-//     }
-//     console.log(`${char}|__${item.name}`);
-//   }
-
-//   if (item.hasOwnProperty('items')) {
-//     tree(item.items, ++str);
-//   } 
-// }
-
 function tree(obj, shift) {
   for (let item of obj) {
       let str = shift === 0 ? 
@@ -45,6 +12,16 @@ function tree(obj, shift) {
       
   }
 }
+
+fs.readFile('./data.json', 'utf8', (err, data) => {
+    if (err) {
+        console.log(`Error reading file from disk: ${err}`);
+    } else {
+        const obj = JSON.parse(data);
+        tree(obj, 0);
+    }
+});
+
   
   
   
