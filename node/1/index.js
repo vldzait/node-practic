96dbb7fb66b215ceaadf48abc70333e0e27d6257
Владимир Зайцев <zaytsev_va@resanta.ru>
2022-01-12 01:24:44 +0300

    [fixed] node-lesson-1

--- node/1/index.js
+++ node/1/index.js
@@ -1,35 +1,50 @@
-let obj = [{
-  name: 1,
-  items: [
-    { name: 2, items: [{ name: 3 }, { name: 4 }] },
-    { name: 5, items: [{ name: 6 }] },
-    { name: 7, items: [{ name: 8, items: [{ name: 9 }] }] },
-  ],
-}];
+const fs = require('fs');
 
-function tree (obj = [], str = 0,) {
-  for (const iterator of obj) {
-    check(iterator, str);
-  }
-}
 
-function check (item , str = 0) {
-  // console.log("_____________________________________");
-  if(str === 0) console.log(`${item.name}`);
-  if(str === 1) console.log(`|__${item.name}`);
-  if(str > 1) {
-    let char = '|';
-    while (char.length < str) {
-      char += '  ';
+fs.readFile('./data.json', 'utf8', (err, data) => {
+    if (err) {
+        console.log(`Error reading file from disk: ${err}`);
+    } else {
+        const obj = JSON.parse(data);
+        tree(obj, 0);
     }
-    console.log(`${char}|__${item.name}`);
-  }
+});
 
-  if (item.hasOwnProperty('items')) {
-    tree(item.items, ++str);
-  } else {
-    return ;
+// function tree (obj = [], str = 0,) {
+
+//   for (const iterator of obj) {
+//     check(iterator, str);
+//   }
+// }
+
+// function check (item , str = 0) {
+//   if(str === 0) console.log(`${item.name}`);
+//   if(str === 1) console.log(`|__${item.name}`);
+//   if(str > 1) {
+//     let char = '|';
+//     while (char.length < str) {
+//       char += '  ';
+//     }
+//     console.log(`${char}|__${item.name}`);
+//   }
+
+//   if (item.hasOwnProperty('items')) {
+//     tree(item.items, ++str);
+//   } 
+// }
+
+function tree(obj, shift) {
+  for (let item of obj) {
+      let str = shift === 0 ? 
+          `|——${"  ".repeat(shift)}${item.name}` :
+          `|${"  ".repeat(shift)}|__${item.name}`;
+      console.log(str);
+      if (item.hasOwnProperty('items')) {
+        tree(item.items, shift+1);
+      } 
+      
   }
 }
-
-tree(obj);
+  
+  
+  
