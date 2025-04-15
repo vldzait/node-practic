6f2d4d866e7101a2c35a35bd7ba6a8a50a4717d5
va_zaytsev <va_zaytsev@betboom.org>
2025-04-15 23:14:20 +0300

    fix: change work algoritm

--- stream/index.js
+++ stream/index.js
@@ -13,7 +13,6 @@
 // ab, cb, bss, cb, b, cb -> [1, 1, 1, 3]
 // alex, alex, juan, dima -> [2, 1, 1]
 
-// const { pipeline } = require('node:stream/promises');
 const { Transform, pipeline } = require('node:stream');
 const fs = require('node:fs');
 
@@ -21,54 +20,74 @@ console.time('Finish');
 
 const sourceFilePath = process.argv[2] ? process.argv[2] : '';
 const resultFilePath = './files/result.txt';
+if (!sourceFilePath) {
+  console.error('Error: No input file specified');
+  process.exit(1);
+}
 const readStream = fs.createReadStream(sourceFilePath);
 const writeStream = fs.createWriteStream(resultFilePath);
 
-// удаляем из строки спец. символы
-function replaceChunk (chunck) {
-  const regx = /[^\w\s]/g;
-  return chunck.replace(regx, '');
-}
 
-// считаем сколько раз встречается каждое слово
-function countWords (row) {
-  const countWordsObj = {};
-  const wordsArr = row.split(/\s+/);
-
-  for (const word of wordsArr) {
-    if (word.length < 1) continue;
-    if (!countWordsObj.hasOwnProperty(word)) {
-      countWordsObj[word] = 0;
-    }
-    countWordsObj[word] += 1;
-  }
-  return countWordsObj;
-}
-
-// сортируем
-function sortCountWords (countWordsObj) {
-  const sortedWords = Object.keys(countWordsObj).sort();
-  return sortedWords.map(word => countWordsObj[word]);
-}
-
-const wordCountStream = new Transform ({
+// заполняем массив слов
+const wordsArr = [];
+const generateWordArrStream = new Transform ({
   transform (chunck, encoding, callback) {
-    chunck = replaceChunk(chunck.toString());
-    const countWordsObj = countWords(chunck); 
-    const resultArr = sortCountWords(countWordsObj);
-    
-    callback(null, JSON.stringify(resultArr));
-  },
+    const regx = /[^\w\s]/g;
+    chunck = chunck.toString().replace(regx, '');
+    wordsArr.push(...chunck.split(/\s+/));
+
+    callback();
+  },flush(callback) {
+    this.push(JSON.stringify(wordsArr));
+    callback();
+  }
 });
 
+// считаем частоту повторов
+const countWordsObj = {};
+const countWordReplayStream = new Transform ({
+  transform (chunck, encoding, callback) {
+    const wordsArr = JSON.parse(chunck.toString());
+    for (const word of wordsArr) {
+      if (word.length < 1) continue;
+      if (!countWordsObj.hasOwnProperty(word)) {
+        countWordsObj[word] = 0;
+      }
+      countWordsObj[word] += 1;
+    }
 
+    callback();
+  },flush(callback) {
+    this.push(JSON.stringify(countWordsObj));
+    callback();
+  }
+});
 
-if (!sourceFilePath) {
-  console.error('undefined file path');
-  return 0;
-}
+// сортируем
+let sortedWords = [];
+const sortStream = new Transform ({
+  transform (chunck, encoding, callback) {
+    sortedWords = Object.keys(countWordsObj).sort().map(word => countWordsObj[word]);
+    
+    callback();
+  },flush(callback) {
+    this.push(JSON.stringify(sortedWords));
+    callback();
+  }
+});
 
-readStream.pipe(wordCountStream).pipe(writeStream);
-
-
-console.timeLog("Finish");
+pipeline(
+  readStream,
+  generateWordArrStream,
+  countWordReplayStream,
+  sortStream,
+  writeStream,
+  (err) => {
+    if (err) {
+      console.error('Pipeline failed:', err);
+    } else {
+      console.timeLog('Finish');
+      console.info('Processing completed');
+    }
+  }
+);
