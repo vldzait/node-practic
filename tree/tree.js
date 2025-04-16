611ab9c01785895228a78ea76627215f857bc26c
va_zaytsev <va_zaytsev@betboom.org>
2025-04-17 00:20:15 +0300

    hw3_feat-write-test-for-tree

--- tree/tree.js
+++ tree/tree.js
@@ -1,19 +1,20 @@
 const fs = require('fs');
 const dir = process.argv[2] ? process.argv[2] : '';
-const maxDepth =  process.argv[4] ? process.argv[4] : Infinity;
+const maxDepth =  process.argv[4] >= 0? process.argv[4] : Infinity;
 
 let folderCount = 0;
 let fileCount = 0;
 
-function dirTree (dir, depth = 0, level = 0) {
-
-  if (dir === '' || !fs.existsSync(dir)) {
+function dirTree (dir, maxDepth = 0, level = 0) {
+  if (level === 0 && (dir === '' || !fs.existsSync(dir))) {
     console.log('Некорректный путь к папке');
     return 0;
   }
 
   const folder = fs.readdirSync(dir);
   if( level === 0) console.log(dir);
+
+  if (level >= maxDepth) return 0;
 
   for (const item of folder) {
     const str = `|${level > 0 ? ' '.repeat(level) : ''}${level > 0 ? '└──' : '──'}${item}`;
@@ -23,12 +24,14 @@ function dirTree (dir, depth = 0, level = 0) {
     } else {
       ++fileCount;
     }
-    if (fs.statSync(`${dir}/${item}`).isDirectory() && depth < maxDepth) {
-      dirTree(`${dir}/${item}`, depth + 1, level + 1);
+    if (fs.statSync(`${dir}/${item}`).isDirectory()) {
+      dirTree(`${dir}/${item}`, maxDepth, level + 1);
     }
   }  
 }
 
 
-dirTree (dir);
-console.log(`${folderCount} directories, ${fileCount} files`);
+dirTree (dir, maxDepth);
+// console.log(`${folderCount} directories, ${fileCount} files`);
+
+module.exports = dirTree;
